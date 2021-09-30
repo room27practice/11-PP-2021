4d70158647894e67e2a17b81598a62f9937c4f74
n_hristov007@abv.bg <Nikolay>
2021-09-30 13:56:26 +0300

    Add

---
+++ OOP/ElektronenDnevnik/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ElektronenDnevnik
+{
+    public class Student
+    {
+        private static int studentCounter = 0;
+        public Student()
+        {
+            studentCounter++;
+            Id = studentCounter;
+            SubjectsGrades = new List<SubjectGrades>();
+        }
+
+        public static string SchoolName { get; } = "PGMETT";
+        public string Name { get; set; }
+        public int Id { get; }
+
+        public List<SubjectGrades> SubjectsGrades { get; set; }
+
+        public List<SubjectGrades> GetHangingSubjects => 
+            SubjectsGrades.Where(x => x.GetGrades().Count() < 3).ToList();
+
+        public List<SubjectGrades> GetFailedSubjects =>
+                SubjectsGrades.Where(x => x.GetRoundedAverage() == 2).ToList();
+
+    }
+}
---
+++ OOP/ElektronenDnevnik/Models/Subject.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace ElektronenDnevnik
+{
+    public class Subject
+    {
+        public Subject(string name, int semesterCount, string description = "")
+        {
+            Name = name;
+            SemesterCount = semesterCount;
+            Description = description;
+        }
+
+        public string Name { get; set; }
+        public int SemesterCount { get; set; }
+        public string Description { get; set; }
+    }
+}
