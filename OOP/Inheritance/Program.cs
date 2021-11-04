0b4a5a460a35451ed4f9b8d8e46712048f5831e8
eng. Hristov <h@abv.bg>
2021-11-04 13:06:37 +0200

    add

---
+++ OOP/Inheritance/Program.cs
@@ -0,0 +1,152 @@
+﻿using Inheritance.Model;
+using System;
+using System.Collections.Generic;
+
+namespace Inheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            // var doggy = new Dog(6);
+            #region inheritanceAbstractDemo    
+            BaseParent t1 = new Tuhla(1, TuhlaSize.chetvorka, Materials.keramic, 23.4);
+            Tuhla t2 = new Tuhla(2, TuhlaSize.chetvorka, Materials.keramic, 23.4);
+            BaseParent b1 = new Diubel(4, 13.4);
+
+            t1.GiveBaseInfo();
+            t2.GiveBaseInfo();
+            b1.GiveBaseInfo();
+
+            //t2.Delete();
+            //b1.Delete();
+            t2.IsDeleted = false;
+            t2.IsDeleted = false;
+            t2.IsDeleted = false;
+            t2.IsDeleted = false;
+            t2.IsDeleted = false;
+
+
+            List<BaseParent> col = new List<BaseParent>();
+            col.Add(t1);
+            col.Add(t2);
+            col.Add(b1);
+
+            List<Tuhla> col2 = new List<Tuhla>();
+            //col2.Add(t1);
+            //col2.Add(t2);
+            //col2.Add(b1);
+
+            Tuhla exp1 = t1 as Tuhla;
+            Console.WriteLine(exp1.Size);
+            Tuhla exp2 = b1 as Tuhla;
+            if (exp2 != null)
+            {
+                Console.WriteLine(exp2.Size);
+            }
+            else
+            {
+                Console.WriteLine("Sorry no casting...");
+            }
+
+            #endregion
+
+
+
+
+
+
+
+
+
+
+
+
+
+            //Animal t1 = new Animal("Marko");
+            //Dog t2 = new Dog("sharo");
+            //t2.Breed = "San Bernar";
+            //Duck t3 = new Duck();
+            //Duck t4 = new Duck();
+
+
+
+            //t3.Breed = "Niskoletqshta";
+            //t2.Weight = 200;
+
+            //List<Animal> animals = new List<Animal>();
+
+            //animals.Add(t2);
+            //animals.Add(t3);
+            //animals.Add(t1);
+
+
+            //Console.WriteLine(((Dog)animals[1]).TailLength);
+
+        }
+    }
+
+    public class Test : Object
+    {
+
+    }
+
+    public class Animal
+    {
+        protected int EyeCount;
+
+        public Animal()
+        {
+            Console.WriteLine("Bezparametrichen konstruktor ot animal se vkliuchi.....");
+        }
+        public Animal(string nme) : this()
+        {
+            Console.WriteLine("Parametrichen konstruktor ot animal se vkliuchi.....");
+            Name = nme;
+        }
+
+        public string Name { get; set; }
+        public string Breed { get; set; }
+        public double Weight { get; set; }
+    }
+
+    public class Dog : Animal
+    {
+        public Dog(int a) : base("rex")
+        {
+            Console.WriteLine("Magichnoto chislo e {0}", a);
+        }
+        public Dog(string nme) : base(nme)
+        {
+            Weight = 0.5;
+        }
+
+        public void Bark()
+        {
+            Console.WriteLine("Woff Woff");
+            Console.WriteLine(Breed);
+        }
+
+        public double TailLength { get; set; } = 99.2;
+    }
+
+
+    public class Duck : Animal
+    {
+        public Duck() : base("kvaklio")
+        {
+            Weight = 0.1;
+        }
+
+        public void Quack()
+        {
+            Console.WriteLine("Quack Quack");
+        }
+    }
+
+
+
+
+
+}
---
+++ OOP/RPG/Program.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RPG
+{
+    class Program
+    {
+        static void Main()
+        {
+            Hero h1 = new Hero("Terminatora_04", Fraction.Priest);
+         //   Console.WriteLine(h1.ItemSet[2]);
+
+            h1.ItemSet.Add(new Item("sword", 8, 0));
+            h1.ItemSet.Add(new Item("cap",0, 6));
+            h1.LevelUp();
+            double heroAtk=  h1.GetAttack();
+            double heroDef=  h1.GetDefence();
+            h1.ItemSet.Add(new Item("sword", 8, 0));
+            h1.ItemSet.Add(new Item("cap", 0, 6));
+            h1.LevelUp();
+            double heroAtk2 = h1.GetAttack();
+            double heroDef2 = h1.GetDefence();
+        }
+    }
+
+    public class Hero
+    {
+        private double attackCoef = 15;
+        private double defenceCoef = 20;
+
+        public Hero(string name, Fraction fraction)
+        {
+            Name = name;
+            Fraction = fraction;
+            ItemSet = new List<Item>();
+        }
+
+        public string Name { get; }
+        public Fraction Fraction { get; set; }
+        public int Level { get; private set; } = 0;
+        public double Health { get; private set; } = 100;
+        public double Mana { get; private set; } = 100;
+        public double Stamina { get; private set; } = 100;
+        public List<Item> ItemSet { get; private set; }
+        public int LevelUp()
+        {
+            Health = 100;
+            Stamina = 100;
+            Mana = 100;
+            Level += 1;
+            return Level;
+        }
+
+        public double GetAttack()
+        {
+            return Level * attackCoef + ItemSet.Sum(x=>x.Attack);
+        }
+
+        public double GetDefence()
+        {
+            return Level * defenceCoef + ItemSet.Sum(x => x.Defence);
+        }
+    }
+
+    public class Item
+    {
+        public Item(string name, double atk, double def)
+        {
+            if (atk < 0 || atk > 10)
+            {
+                Console.WriteLine("Invalid Attack item value");
+                Attack = 9;
+            }
+            else
+            {
+                Attack = atk;
+            }
+
+            if (def < 0 || def > 10)
+            {
+                Console.WriteLine("Invalid Defence item value");
+                Defence = 9;
+            }
+            else
+            {
+                Defence = def;
+            }
+
+            Name = name;
+        }
+
+        public string Name { get; }
+        public double Attack { get; private set; }
+        public double Defence { get; private set; }
+
+        public void UpgradeAttack(double up)
+        {
+            if (up < 0 || up > 50)
+            {
+                Console.WriteLine("Invalid Attack improvement value");
+            }
+            else
+            {
+                Attack += up;
+                //Attack = Attack + up; равнозначно.
+            }
+        }
+
+        public void UpgradeDefence(double up)
+        {
+            if (up < 0 || up > 50)
+            {
+                Console.WriteLine("Invalid Defence improvement value");
+            }
+            else
+            {
+                Defence += up;
+                //Defence = Defence + up; равнозначно.
+            }
+        }
+    }
+
+    public enum Fraction
+    {
+        Mage,
+        Knight,
+        Priest,
+        Ranged,
+        Rogue
+    }
+}
