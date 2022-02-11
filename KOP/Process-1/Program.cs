2c48a53198dc84a7c561f1a00983603cf17faa20
a <b>
2022-02-11 10:51:02 +0200

    KOP1

---
+++ KOP/Process-1/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Numerics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Process_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int threads = Environment.ProcessorCount;
+            Console.WriteLine(threads);
+            var cpuInfo = Environment.GetEnvironmentVariable("PROCESSOR_IDENTIFIER");
+            Console.WriteLine(cpuInfo);
+
+
+            Stopwatch stopWatch = new Stopwatch();
+
+
+            stopWatch.Start();
+            BigInteger sum_a = SumAllEvenNumbersAssinc(0, 100);
+            stopWatch.Stop();
+            BigInteger sum_b = GetSumOfEvenNumbers(0, 100);
+            var res = sum_a - sum_b;
+            var timeSpent = stopWatch.ElapsedMilliseconds;
+            Console.WriteLine(timeSpent);
+            Console.WriteLine(sum_a);
+        }
+
+        private static BigInteger SumAllEvenNumbersAssinc(int start, int end)
+        {//0-100
+            int cpuCount = Environment.ProcessorCount-1;
+            BigInteger sum = 0;
+            int portion = (end - start) / cpuCount;
+            Task[] tasks = new Task[cpuCount];
+            for (int i = 0; i < cpuCount; i++)
+            {
+                tasks[i] = new Task(() => sum += GetSumOfEvenNumbers(start + portion * i, portion * (i + 1)));
+                tasks[i].Start();
+            }
+
+            Task.WaitAll(tasks);
+
+            return sum;
+        }
+
+
+        private static BigInteger GetSumOfEvenNumbers(int start, int end)
+        {
+            BigInteger sum = 0;
+            for (int i = start; i < end; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ OOP/AnonymousDemo/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnonymousDemo
+{
+    class Program
+    {
+        const int TOTAL_SCORE = 81;
+        static void Main(string[] args)
+        {
+            var input = @"1 Бирол 56
+2 Даниел 0
+3 Елена 0
+4 Ерсин 51
+5 Есер 33
+6 Ивайло 51
+7 Красимир 41
+8 Нели 55
+9 Петко 32
+10 Пламен 26
+11 Сезгин 39
+12 Селим 59
+13 Сечкин 69";
+
+            var test = input
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            List<Student> students = input
+                .Split(Environment.NewLine, StringSplitOptions.RemoveEmptyEntries)
+                .Select(x => new Student(x))
+                .ToList();
+
+            foreach (Student st in students.OrderByDescending(x => GetGradeFromPoints(x.Points)).Where(x => x.Points > 0))
+            {
+                Console.WriteLine($"{st.Number}-{st.Name}-{GetGradeFromPoints(st.Points)}");
+            }
+
+            var anon1 = new { Name = "Genadi", GSM = "094323234542", Town = "Shumen" };
+            var anon2 = new { Neim = "Genadi", GSM = "094323234542", Town = "Shumen" };
+            var experiment = students.Select(s => new { s.Number, s.Name }).ToArray();
+
+            foreach (var item in experiment)
+            {
+                Console.WriteLine(item.Name + " " + item.Number);
+            }
+        }
+
+        public static int GetGradeFromPoints(int points) =>
+                (int)Math.Round((2 + Math.Ceiling(4 * points / TOTAL_SCORE / 0.25) * 0.25));
+    }
+
+    public class Student
+    {
+        public Student(string input)
+        {
+            //"1 Birol 56"
+            string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Number = int.Parse(tokens[0]);
+            Name = tokens[1];
+            Points = int.Parse(tokens[2]);
+        }
+
+        public Student(int number, string name, int points)
+        {
+            Number = number;
+            Name = name;
+            Points = points;
+        }
+
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+    }
+}
